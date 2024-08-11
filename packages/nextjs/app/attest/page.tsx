afd2e82d8b253337510b2b2c726e14544d2a92c2
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2024-08-11 07:33:25 -0700

    update form

--- packages/nextjs/app/attest/page.tsx
+++ packages/nextjs/app/attest/page.tsx
@@ -108,7 +108,7 @@ const SkillRatingForm = () => {
             name="address"
             value={formData.address}
             onChange={handleChange}
-            placeholder="Enter your address"
+            placeholder="Enter address to Attest"
             className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           ></textarea>
         </div>
